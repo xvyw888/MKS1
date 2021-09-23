70c1fac5058d679e4a76c89c7e4ecc1afaa10fa4
xvyw888 <vzboril@email.cz>
2021-09-23 12:22:49 +0200

    cviceni 1 - 2.4. morseovka

--- MKS-1/Src/main.c
+++ MKS-1/Src/main.c
@@ -30,15 +30,28 @@ int main(void)
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
 
-	uint8_t pole[] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
+	uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
 
     /* Loop forever */
-	for(;;)
+	while(1){
+	for(uint8_t i = 0; i < 32; i++)
 	{
+		if (pole[i] == 1)
+		{
 
-		GPIOA->BSRR = (1<<5);  /* set */
-		GPIOA->BRR = (1<<5);   /* reset */
-		GPIOA->ODR ^= (1<<5);  /* toggle */
-		for (volatile uint32_t i = 0; i < 100000; i++) {}
+			GPIOA->BSRR = (1<<5);
+
+		}
+
+		else
+
+		{
+
+				GPIOA->BRR = (1<<5);
+		}
+
+		for (volatile uint32_t i = 0; i < 1000000; i++) {}
+	}
 	}
 }
+
